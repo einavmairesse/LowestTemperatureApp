5853b153026c04104738eddd4747569895841463
einavmairesse <einavmyress22@gmail.com>
2020-12-15 15:31:22 +0200

    Change from "last" 5 day to "next" five days.

--- App/routes.py
+++ App/routes.py
@@ -1,14 +1,16 @@
 from flask import jsonify
-
+from flask_cors import CORS
 from App import app, config
 from App.services import get_coldest_city_context
 
 
+CORS(app)
+
+
 @app.route('/get_lowest_temp', methods=['GET'])
 def get_lowest_temp():
-    cities = config.city_to_coordinates
-
-    coldest_city_context = get_coldest_city_context(cities)
+    cities = config.cities
+    coldest_city_context = get_coldest_city_context()
     return jsonify(coldest_city_context), 200
 
 
