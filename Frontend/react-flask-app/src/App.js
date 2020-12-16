3a7d95893b9312d2fd9a010b6576d2072e61fad6
einavmairesse <einavmyress22@gmail.com>
2020-12-16 14:42:25 +0200

    test1 json fetched and saved

--- Frontend/react-flask-app/src/App.js
+++ Frontend/react-flask-app/src/App.js
@@ -1,31 +1,20 @@
-import React, { useState, useEffect } from 'react';
+
 import './App.css';
 
-function App() {
-  const [weatherData, setWeatherData] = useState([]);
-
-  useEffect(() => {
-    // GET request using fetch inside useEffect React hook
-    fetch('http://127.0.0.1:5000/get_lowest_temp')
-        .then(response => response.json())
-        .then(r => setWeatherData(r));
-// empty dependency array means this effect will only run once (like componentDidMount in classes)
-}, []);
-
-
-
-  return (
-    <div className="App">
-      <header className="App-header">
-
-        hi ...
-
-        <p>city_name: {weatherData.city_name}.</p>
-        <p>feels_like: {weatherData.feels_like}.</p>
-        <p>grnd_level: {weatherData.grnd_level}.</p>
-      </header>
-    </div>
-  );
+ function App() {
+  let weatherData = fetchWeatheData();
+  console.log(weatherData);
+ 
+  return <p> einav</p>;
 }
+
+
+
+async function fetchWeatheData() {
+  const response = await fetch ('http://127.0.0.1:5000/get_lowest_temp');
+  const weatherData = await response.json();
+  return weatherData;
+}
+
 
 export default App;
