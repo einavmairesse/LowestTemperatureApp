3dceb39e485ffb3a08097d37417098506031cd73
einavmairesse <einavmyress22@gmail.com>
2020-12-16 17:53:17 +0200

    Iterate over json -succsess

--- Frontend/react-flask-app/src/App.js
+++ Frontend/react-flask-app/src/App.js
@@ -1,19 +1,36 @@
 
 import './App.css';
+import React, { Component } from "react";
 
- function App() {
-  let weatherData = fetchWeatheData();
-  console.log(weatherData);
- 
-  return <p> einav</p>;
-}
+class App extends Component {
+  constructor() {
+    super();
+    this.state = { data: [] };
+  }
 
+  async componentDidMount() {
+    const response = await fetch('http://127.0.0.1:5000/get_lowest_temp');
+    const json = await response.json();
+    this.setState({ data: json });
+  }
 
+  render() {
+    var text = ""
+    console.log(this.state.data)
+    for( let prop in this.state.data ){
+      text = text + "<li>" + prop + ":" + this.state.data[prop] + "</li>" ; 
+    }
 
-async function fetchWeatheData() {
-  const response = await fetch ('http://127.0.0.1:5000/get_lowest_temp');
-  const weatherData = await response.json();
-  return weatherData;
+    console.log(text)
+    
+    return (
+      <div>
+        <ul>
+        {text}
+         </ul>
+      </div>
+    );
+  }
 }
 
 
