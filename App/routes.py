54cd9815969e139f03d9c4f30355861489e1784f
einavmairesse <einavmyress22@gmail.com>
2020-12-17 14:31:39 +0200

    App is ready

--- App/routes.py
+++ App/routes.py
@@ -1,16 +1,16 @@
 from flask import jsonify
 from flask_cors import CORS
 from App import app, cache
-from App.services import get_coldest_city_context, get_next_flush
+from App.services import get_coldest_city_content, get_next_cache_flush
 
 CORS(app)
 
 
 @app.route('/get_lowest_temp', methods=['GET'])
-@cache.cached(timeout=get_next_flush())
+@cache.cached(timeout=get_next_cache_flush())
 def get_lowest_temp():
-    coldest_city_context = get_coldest_city_context()
-    return jsonify(coldest_city_context), 200
+    coldest_city_content = get_coldest_city_content()
+    return jsonify(coldest_city_content), 200
 
 
 if __name__ == '__main__':
